132e4b48a9451180279f41bf308a2bbf595d5e27
laurammberk <laurammberk3105@gmail.com>
2026-08-08 20:25:26 +0000

    added wait

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ resource "helm_release" "this" {
   name       = var.name
   repository = var.repository
   chart      = var.chart
-  namespace = var.namespace
+  namespace  = var.namespace
+  wait       = var.wait   #release my terminal, let me troubleshoot
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,28 @@
 variable "name" {
-    description = "Please specify a name for your deployment"
-    type = string
-    default = "demo"
+  description = "Please specify a name for your deployment"
+  type        = string
+  default     = "demo"
 }
 
 variable "namespace" {
-    description = "Please specify a namespace for your deployment"
-    type = string
-    default = null
+  description = "Please specify a namespace for your deployment"
+  type        = string
+  default     = "default"
 }
 
 variable "repository" {
-    description = "Please specify a repository for your deployment"
-    type = string
-    default = null
+  description = "Please specify a repository for your deployment"
+  type        = string
+  default     = null
 }
 
 variable "chart" {
-    description = "Please specify a chart for your deployment"
-    type = string
+  description = "Please specify a chart for your deployment"
+  type        = string
+}
+
+variable "wait" {
+  description = "Please specify if you want to wait or not"
+  type        = bool
+  default     = false
 }
